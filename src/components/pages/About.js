ebb307d9e5587f81aa4c8049d94a524f1557c644
rein96 <reinhartandreas96@gmail.com>
2019-09-05 20:26:53 +0700

    https://reinhartandreas-githubfinder.netlify.com/

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,6 +4,8 @@ const About = () => {
     return (
         <Fragment>
             <h2>About this App</h2>
+            <p> Created by Reinhart Andreas, learned from Brad Traversy Udemy Course. </p>
+            <p> You can see the code here: https://github.com/rein96/Github-Finder </p>
             <p> App to search Github users</p>
             <p> Version: 1.0.0 </p>
         </Fragment>
